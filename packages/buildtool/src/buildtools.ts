b95ce0aef9f37f19f887e36db4711f940b5bbdf8
Lukas Bach <lbach@outlook.de>
2022-04-16 17:45:55 +0200

    reduce buildtool output

--- packages/buildtool/src/buildtools.ts
+++ packages/buildtool/src/buildtools.ts
@@ -3,14 +3,14 @@
 import { Documentalist, TypescriptPlugin } from "@documentalist/compiler";
 import { ensureDir, remove, writeJson, readdir, lstatSync } from "fs-extra";
 import { join } from "path";
-import { program } from 'commander';
-import { compress } from 'compress-json';
-import download from 'download-tarball';
+import { program } from "commander";
+import { compress } from "compress-json";
+import download from "download-tarball";
 
-const tmpPath = `./tmp`;
+const tmpPath = "./tmp";
 
 const scanFolderStructure = async (currentFolder = tmpPath) => {
-  let currentObj = {};
+  const currentObj = {};
   const files = await readdir(currentFolder);
   for (const file of files) {
     if (lstatSync(join(currentFolder, file)).isDirectory()) {
@@ -20,14 +20,16 @@ const scanFolderStructure = async (currentFolder = tmpPath) => {
     }
   }
   return currentObj;
-}
+};
 
 program
   .argument("<package>", "package name")
   .argument("<version>", "package version")
   .argument("<target>", "target folder")
   .action(async (packageName: string, version: string, target: string) => {
-    const packageNameWithoutScope = packageName.includes("@") ? packageName.split("/", 2)[1] : packageName;
+    const packageNameWithoutScope = packageName.includes("@")
+      ? packageName.split("/", 2)[1]
+      : packageName;
     await download({
       url: `https://registry.npmjs.org/${packageName}/-/${packageNameWithoutScope}-${version}.tgz`,
       dir: tmpPath,
@@ -40,9 +42,15 @@ program
     await writeJson(join(target, "folder.json"), folderStructureCompressed);
 
     const docs = await new Documentalist()
-      .use(/\.tsx?$/, new TypescriptPlugin({ verbose: true, includeDeclarations: true }))
+      .use(
+        /\.tsx?$/,
+        new TypescriptPlugin({
+          verbose: true,
+          includeDeclarations: true,
+          excludePaths: ["^\\.\\."],
+        })
+      )
       .documentGlobs("tmp/**/*");
-
 
     const compressed = compress(JSON.parse(JSON.stringify(docs)));
     await writeJson(join(target, "docs.json"), compressed);
